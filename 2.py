6e303d5a3385932aa8449a3310e0e25e4b32f422
2pokek <alkuzmich6479@gmail.com>
2022-12-11 23:20:27 +0300

    3

--- 2.py
+++ 2.py
@@ -2,4 +2,6 @@ import pyAesCrypt
 
 password = input('Enter password: ')
 
-pyAesCrypt.encryptFile('random.txt','random.txt.aes',password)
+# pyAesCrypt.encryptFile('random.txt','random.txt.aes',password)
+
+pyAesCrypt.decryptFile('random.txt.aes','derandom.txt',password)
